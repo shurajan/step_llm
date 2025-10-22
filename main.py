b927d2ac15ba27a91073a353eca4c2ea899ffdba
Alexander Bralnin <sasha@v869649920.local>
2025-10-22 13:02:57 +0000

    new enviroment

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
 from bpe import BPE
-import torch
-
-device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-print("Using:", device)
-
-# простая проверка вычислений на MPS
-x = torch.randn(1000, 1000, device=device)
-y = torch.randn(1000, 1000, device=device)
-z = (x @ y).sum()
-z.backward() if z.requires_grad else None
-print("OK")
 
 # 🔹 Пример использования
 bpe = BPE(vocab_size=31)
@@ -23,10 +12,7 @@ print(encoded)
 
 print(bpe.decode(encoded))
 
-bpe.save('data/bpe.dill')
-bpe2 = BPE.load('data/bpe.dill')
+bpe.save("data/bpe.dill")
+bpe2 = BPE.load("data/bpe.dill")
 
 print(bpe2.tokens)
-
-# print(bpe.get_token_to_id())
-# print(bpe.get_id_to_token())
